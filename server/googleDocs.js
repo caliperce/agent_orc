371e01149cb469ce268b381285d7d44d77df8c2d
caliperce <128280700+caliperce@users.noreply.github.com>
2025-05-25 15:36:46 +0530

    credentials are corrected

--- server/googleDocs.js
+++ server/googleDocs.js
@@ -7,7 +7,7 @@ const auth = new google.auth.GoogleAuth({
         type: process.env.GOOGLE_TYPE,
         project_id: process.env.GOOGLE_PROJECT_ID,
         private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        private_key: process.env.PRIVATE_KEY.split(String.raw`\n`).join('\n'),
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_CLIENT_ID,
         auth_uri: process.env.GOOGLE_AUTH_URI,
